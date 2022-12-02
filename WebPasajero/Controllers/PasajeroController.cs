c43487a03e1ae28d0a53b26da1113babba664360
Federico Fantacone <fede26f@hotmail.com>
2022-12-02 13:47:34 -0300

    Agregar archivos de proyecto.

---
+++ WebPasajero/Controllers/PasajeroController.cs
@@ -0,0 +1,67 @@
+﻿using EntityFrameworkExample.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using WebPasajero.Models;
+
+namespace WebPasajero.Controllers
+{
+    public class PasajeroController : Controller
+    {
+        private readonly PasajeroContext _context;
+
+        public PasajeroController(PasajeroContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Pasajeros.ToList());
+        }
+
+
+        //GET: /Pasajero/Create
+        public IActionResult Create()
+        {
+            Pasajero pasajero = new Pasajero();
+            return View("Create", pasajero);
+        }
+
+        //POST: /Pasajero/Create
+        [HttpPost]
+        public IActionResult Create(Pasajero pasajero)
+        {
+            _context.Add(pasajero);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpGet("/pasajero/fecha/{fecha}")]
+        // GET: 
+        public IActionResult ListaPorFecha(DateTime fecha)
+        {
+            List<Pasajero> lista = (from p in _context.Pasajeros
+                                  where p.FechaDeNacimiento == fecha
+                                  select p).ToList();
+            return View("Index", lista);
+        }
+
+
+
+        [HttpGet("/pasajero/ciudad/{city}")]
+        // GET: 
+        public IActionResult ListaPorCiudad(string city)
+        {
+            List<Pasajero> lista = (from p in _context.Pasajeros
+                                    where p.Ciudad == city
+                                    select p).ToList();
+            return View("Index", lista);
+        }
+
+
+    }
+}
---
+++ WebPasajero/Data/PasajeroContext.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using WebPasajero.Models;
+
+namespace EntityFrameworkExample.Data
+{
+
+    public class PasajeroContext : DbContext
+    {
+        public PasajeroContext(DbContextOptions<PasajeroContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pasajero> Pasajeros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //OPCIONAL INICIALIZAR LA BASE DE DATOS CON 2 PERSONAS
+            modelBuilder.Entity<Pasajero>().HasData(
+               new Pasajero
+               {
+                   Id = 1,
+                   Nombre = "Tara",
+                   Apellido = "Brewer",
+                   FechaDeNacimiento = new DateTime(2015, 7, 8, 6, 34, 53),
+                   Ciudad = "LosAngeles"
+               },
+               new Pasajero
+               {
+                   Id = 2,
+                   Nombre = "Andrew",
+                   Apellido = "Tippett",
+                   FechaDeNacimiento = new DateTime(2016, 6, 1, 6, 34, 53),
+                   Ciudad = "NickelRoad"
+               });
+        }
+    }
+}
